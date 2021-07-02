f45c4624c37aa9edbae342b37abd3ab2c2ed61c2
Joseph Williams <josephwilliamsjs@gmail.com>
2021-07-01 21:02:13 -0700

    create signup form

--- client/src/components/SignUpPage.tsx
+++ client/src/components/SignUpPage.tsx
@@ -1,6 +1,61 @@
+import { useState } from 'react'
+
 const SignUpPage = () => {
+  const [organization, setOrganization] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [plan, setPlan] = useState('')
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+  }
+
   return(
+    <div>
     <h1>Sign up</h1>
+    <form onSubmit={(event => handleSubmit(event))}>
+    <input 
+      type="text" 
+      value={organization} 
+      onChange={e => setOrganization(e.target.value)}
+      placeholder="organization"
+    />
+    <input 
+      type="text" 
+      value={email} 
+      onChange={e => setEmail(e.target.value)}
+      placeholder="email"
+    />
+    <input 
+      type="password" 
+      value={password} 
+      onChange={e => setPassword(e.target.value)}
+      placeholder="password"
+    />
+    <input 
+      type="text" 
+      value={firstName} 
+      onChange={e => setFirstName(e.target.value)}
+      placeholder="first name"
+    />    
+    <input 
+      type="text" 
+      value={lastName} 
+      onChange={e => setLastName(e.target.value)}
+      placeholder="last name"
+    />    
+    <select value={plan} onChange={e => setPlan(e.target.value)}>
+        <option value="free">Free</option>
+        <option value="paid">Paid</option>
+    </select>
+    <input className='button' 
+      type="submit"
+      value="register"
+    />
+  </form>
+    </div>
   )
 }
 export default SignUpPage
